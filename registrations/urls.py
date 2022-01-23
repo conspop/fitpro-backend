095b3123b5acac27afcf860f64c36cf56e0e3d0d
Seb <you@example.com>
2022-01-23 15:39:47 -0500

    Add views to Registrations

--- registrations/urls.py
+++ registrations/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import RegisterListCreate, RegistrationRetrieveUpdateDestroy
 
 urlpatterns = [
-
+    path("", RegisterListCreate.as_view()),
+    path("<int:pk>", RegistrationRetrieveUpdateDestroy.as_view())
 ]
--- registrations/views.py
+++ registrations/views.py
@@ -1,5 +1,20 @@
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import RegistrationSerializer
 
+from rest_framework import generics
 
-class Registrations(APIView):
-    pass
+class RegisterListCreate(generics.ListCreateAPIView):
+    serializer_class = RegistrationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.registrations.all()
+
+class RegistrationRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RegistrationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.registrations.all()
